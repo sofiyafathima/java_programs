c60058a36c015c154628d0de1c3949f3a26fac6c
sofiyafathima <36063914+sofiyafathima@users.noreply.github.com>
2018-09-05 09:51:38 +0600

    Add files via upload

---
+++ Num.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package num;
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Num{
+
+    
+   public static void main(String[] args) {
+       Scanner s = new Scanner(System.in);
+      int num;
+       num = s.nextInt();
+       if(num<=0)
+    {
+        System.out.println(num+"Negative");
+    }
+ 
+ else
+ {
+     System.out.println("enter the correct number");
+ }
+ }
+    }
+
+    
+       
+    
+
