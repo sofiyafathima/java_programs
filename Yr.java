15047c7b8901dfa59c530d91b6b46029effd80c2
sofiyafathima <36063914+sofiyafathima@users.noreply.github.com>
2018-09-14 14:49:54 +0530

    Update Yr.java

--- Yr.java
+++ Yr.java
@@ -1,16 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package yr;
  import java.util.*;
 import java.io.*;
 import java.lang.*;
 
-/**
- *
- * @author MSAJCE-CSE31
- */
+
 public class Yr {
 
  
@@ -27,7 +21,7 @@ public class Yr {
      }
       else 
       {
-          System.out.println("it is not leap year");// TODO code application logic here
+          System.out.println("it is not leap year");
     }
 }
 }
