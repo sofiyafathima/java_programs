127df53c56784de45215ab06bec4987f3664052a
sofiyafathima <36063914+sofiyafathima@users.noreply.github.com>
2018-09-18 13:58:32 +0530

    Update Alpha.java

--- Alpha.java
+++ Alpha.java
@@ -1,17 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package alpha;
 
 import java.util.*;
 import java.io.*;
 import java.lang.*;
 
-/**
- *
- * @author MSAJCE-CSE31
- */
+
 public class Alpha {
    
     
