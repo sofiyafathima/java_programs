f0e7f203f59140dcdf1587aed5b603d4b3efa2f3
sofiyafathima <36063914+sofiyafathima@users.noreply.github.com>
2018-09-18 14:15:41 +0530

    Add files via upload

---
+++ Larg.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package larg;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+/**
+ *
+ * @author MSAJCE-CSE31
+ */
+public class Larg {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1=20,num2=40,num3=50;
+       Scanner s = new Scanner(System.in);
+     int num = s.nextInt();
+     if(num1>=num2 && num1>=num3)
+     {
+         System.out.println(num1+"is a largest number");
+     }
+     if else(num2>=num1 && num2>=num3)
+     {
+         System.out.println(num2+"is a largest number");
+     }
+     else
+    {
+        System.out.pritnln(num3+"is a largest number");
+    }
+}
+
+     
+       
+       
+    
+
