1520529bd3e63276ad9b091d3212c90c226b5220
sofiyafathima <36063914+sofiyafathima@users.noreply.github.com>
2018-09-24 14:52:30 +0530

    Add files via upload

---
+++ Eveinterval.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Eveinterval {
+
+    
+    public static void main(String[] args) {
+        int n  ,i,t;
+      Scanner s = new Scanner(System.in);
+       n = s.nextInt();
+       t=s.nextInt();
+  for(i=t;i<=n;i++)
+      {
+      if(i%2==0)
+      {
+           System.out.println(i);
+      }
+          
+      
+      }
+    }
+}
+    
+
