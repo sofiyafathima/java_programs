648416b57b207d564a2e1200af9359dc5e05a04f
sofiyafathima <36063914+sofiyafathima@users.noreply.github.com>
2018-09-24 13:46:14 +0530

    Update Expo.java

--- Expo.java
+++ Expo.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package expo;
 import java.io.*;
 import java.util.*;
 import java.lang.*;
-/**
- *
- * @author MSAJCE
- */
+
 public class Expo {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         int base = 2 , exponent =3 ;
        Scanner s = new Scanner(System.in);
