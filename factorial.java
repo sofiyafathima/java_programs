fbdf937b9727420ee8e795c9544351fd4d747550
sofiyafathima <36063914+sofiyafathima@users.noreply.github.com>
2018-09-26 07:02:02 -0700

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class factorial{
+ public static void main(String []args){
+ int i,num=5,fact=1;
+ 
+ for(i=1;i<=num;i++)
+         {
+             fact = fact*i;
+         }
+             System.out.println(num+"factorial 0f a number"+fact);
+         
+        
+     }
+}
