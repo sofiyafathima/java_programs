086322b4e09f003d07e710014ee0cb2047f0c749
sofiyafathima <36063914+sofiyafathima@users.noreply.github.com>
2018-09-29 14:10:05 +0530

    Create primenot.java

---
+++ primenot.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+
+public class Main {
+   
+    public static void main(String[] args) {
+        int num=1222,i;
+        boolean flag = false;
+       
+        for(i=2; i<=num/2;i++)
+        {
+
+           if( num %i==0)
+               {
+            flag = true;
+            break;
+
+            }
+        }
+        if(!flag)
+        {
+            System.out.println(num+"is a prime number");
+        }
+        else{
+            System.out.println(num+"is not a prime number");
+        }
+    }
+}
+      
