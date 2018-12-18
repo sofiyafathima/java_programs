70affaf29846b2dd58505ac121a181c73b5b502e
sofiyafathima <36063914+sofiyafathima@users.noreply.github.com>
2018-12-18 11:37:23 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,38 @@
+package prime.java;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+/**
+ *
+ * @author MSAJCE
+ */
+public class PrimeJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int num1,num2,i;
+       Scanner s = new Scanner(System.in);
+       num1 = s.nextInt();
+       num2 = s.nextInt();
+       while(num1<num2)
+       {
+       boolean flag = false;
+       for(i=2;i<=num1/2;i++){
+           if(num1%2==0)
+           {
+           flag = true;
+            break;
+           }
+    }
+       
+       if(!flag)
+       {
+         System.out.println(num1+"");
+         ++num1;
+       }
+    }
+    }
+}
